a10a3f323bc124043677d850d0df8e817441ac15
Coookies <simranf996@gmail.com>
2024-08-08 15:38:35 -0700

    Customizing site and adding articles.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Coookies';
-export const SITE_DESCRIPTION = 'Let\'s start eating coookies!';
+export const SITE_DESCRIPTION = 'Coookie Eater\'s official website!';
--- src/content/config.ts
+++ src/content/config.ts
@@ -13,4 +13,18 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+
+const techbytes = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { blog, techbytes };
