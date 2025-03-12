2bec32e9323bd2bee5464da8016fb5d1dbe92713
Simran Farheen <simranf996@gmail.com>
2025-03-12 16:19:40 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://coookieeater.github.io',
+	site: 'https://simranfarheen.github.io',
 	integrations: [mdx(), sitemap()],
 });
